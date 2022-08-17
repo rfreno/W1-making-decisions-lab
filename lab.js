5f52def266d35e87d894ba61b7912d4df4c625d6
rfreno <wintersbek@gmail.com>
2022-08-17 15:12:10 -0600

    lab complete

--- lab.js
+++ lab.js
@@ -103,19 +103,26 @@ for (let i = 0; i < 10; i++) {
 ////////// PROBLEM 6 //////////
 
 /*
-  Create a for-loop that runs 10 times, logging the numbers 1 through 10. Ex, on the first iteration it would log 1, on the second iteration it would log 2.
+  Create a for-loop that runs 10 times, logging the numbers 1
+   through 10. Ex, on the first iteration it would log 1, on the 
+   second iteration it would log 2.
 */
 
-//CODE HERE
+for (let i = 0; i < 10; i++) {
+  console.log(i + 1);
+}
 
 ////////// PROBLEM 7 //////////
 
 /*
-  Create a for-loop that logs the numbers 0 through 10 backwards. Ex, on the first iteration it would log 10, on the second iteration it would log 9.
+  Create a for-loop that logs the numbers 0 through 10 backwards. 
+  Ex, on the first iteration it would log 10, on the second 
+  iteration it would log 9.
 */
 
-//CODE HERE
-
+for (let i = 10; i > -1; i--) {
+  console.log(i);
+}
 
 ////////// PROBLEM 8 //////////
 
@@ -123,32 +130,63 @@ let score = 0;
 let passingScore = 7;
 
 /*
-  Create a while-loop that logs "Your score is not high enough" while the score variable above is below passingScore. In each iteration of the loop, increase the score by one. If your code is successful, you will see your logged statement 7 times.
+  Create a while-loop that logs "Your score is not high enough" 
+  while the score variable above is below passingScore. In each 
+  iteration of the loop, increase the score by one. If your code 
+  is successful, you will see your logged statement 7 times.
 */
 
-//CODE HERE
+while (score < passingScore) {
+  console.log("Your score is not high enough.");
+  score++;
+}
 
 ////////// INTERMEDIATE PROBLEMS //////////
 
 ////////// PROBLEM 9 //////////
 /*
   Create a variable called 'changeMyMind' and set it equal to true. 
-  Check to see if changeMyMind is set to true or false, if it is true, change the status to false, if it is false, change the status to true.
+  Check to see if changeMyMind is set to true or false, if it is 
+  true, change the status to false, if it is false, change the 
+  status to true.
 
 */
 
-//CODE HERE
+let changeMyMind = true;
+
+if (changeMyMind === true) {
+  changeMyMind = false
+} else {
+  changeMyMind = true
+}
 
 ////////// PROBLEM 10 //////////
-// Using the "not" operator (!), change the current value of changeMyMind to true (it should currently be set to false due to the if-else statement in Problem 9). After you change the value of changeMyMind, console.log it's new value (it should now read true).
+/* Using the "not" operator (!), change the current value of 
+changeMyMind to true (it should currently be set to false due to 
+  the if-else statement in Problem 9). After you change the value 
+  of changeMyMind, console.log it's new value (it should now read 
+    true).
+    */
 
-//CODE HERE
+    if (changeMyMind !== true) {
+      changeMyMind = true;
+    }
 
 ////////// ADVANCED PROBLEMS //////////
 
 ////////// PROBLEM 11 //////////
 
 let z = 5;
-// Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
+/* Create a while loop that continues to run while z is greater 
+than 0. Within the while loop, log a countdown from the value of z 
+to 1. Once you have logged the countdown, make sure you decrement z 
+by 1. If your code is running properly, you should see: 
+5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
+*/ 
 
-//CODE HERE
+while (z > 0) {
+  for (let i = z; i > 0; i--) {
+    console.log(i);
+  }
+  z--;
+}
